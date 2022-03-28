9796839ce141b4c0e6f5e097e2e0ec656ef55271
Diego Soares Moreira <soares.diego95@hotmail.com>
2022-03-28 18:36:32 -0300

    Association

---
+++ src/academy/devdojo/javaoneforall/javacore/Gassociation/test/PlayerTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.javaoneforall.javacore.Gassociation.test;
+
+import academy.devdojo.javaoneforall.javacore.Gassociation.domain.Player;
+
+public class PlayerTest01 {
+    public static void main(String[] args) {
+        Player player1 = new Player("Pelé");
+        Player player2 = new Player("Romário");
+        Player player3 = new Player("Cafú");
+
+        Player[] players = {player1, player2, player3};
+
+        for(Player player: players) {
+            player.print();
+        }
+    }
+}
